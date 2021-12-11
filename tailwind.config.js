6080018ba8eaca6fb073ca0dea54fc4624be74d7
EyePatch128 <46873888+EyePatch128@users.noreply.github.com>
2021-12-11 19:00:26 +0100

    missions added; styles updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,8 +19,8 @@ module.exports = {
       },
       textShadow:{
         "retro-gray" : "0.5px 0.5px #f1f4d8, 1px 1px #f1f4d8, 2px 2px #f1f4d8, 2.5px 2.5px #242423, 3px 3px #242423, 3.5px 3.5px #242423, 4px 4px #242423, 4.5px 4.5px #242423, 5px 5px #242423, 5.5px 5.5px #242423, 6px 6px #242423, 6.5px 6.5px #242423, 7px 7px #242423, 7.5px 7.5px #242423;",
-        "retro-green": "0.5px 0.5px #f1f4d8, 1px 1px #f1f4d8, 2px 2px #f1f4d8, 2.5px 2.5px #047857, 3px 3px #047857, 3.5px 3.5px #047857, 4px 4px #047857, 4.5px 4.5px #047857, 5px 5px #047857, 5.5px 5.5px #047857, 6px 6px #047857, 6.5px 6.5px #047857, 7px 7px #047857, 7.5px 7.5px #047857;",
-        "retro-green-sm": "0.5px 0.5px #f1f4d8, 1px 1px #f1f4d8, 2px 2px #f1f4d8, 2.5px 2.5px #047857, 3px 3px #047857, 3.5px 3.5px #047857, 4px 4px #047857, 4.5px 4.5px #047857, 5px 5px #047857, 5.5px 5.5px #047857, 6px 6px #047857, 6.5px 6.5px #047857;"
+        "retro-green": "0.5px 0.5px #f1f4d8, 1px 1px #f1f4d8, 2px 2px #f1f4d8, 2.5px 2.5px #16A34A, 3px 3px #16A34A, 3.5px 3.5px #16A34A, 4px 4px #16A34A, 4.5px 4.5px #16A34A, 5px 5px #16A34A, 5.5px 5.5px #16A34A, 6px 6px #16A34A, 6.5px 6.5px #16A34A, 7px 7px #16A34A, 7.5px 7.5px #16A34A;",
+        "retro-green-sm": "0.5px 0.5px #f1f4d8, 1px 1px #f1f4d8, 2px 2px #f1f4d8, 2.5px 2.5px #16A34A, 3px 3px #16A34A, 3.5px 3.5px #16A34A, 4px 4px #16A34A, 4.5px 4.5px #16A34A, 5px 5px #16A34A, 5.5px 5.5px #16A34A, 6px 6px #16A34A, 6.5px 6.5px #16A34A;"
         
       }
     },
